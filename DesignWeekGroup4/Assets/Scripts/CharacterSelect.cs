e5c99a7c48ae37fd5b285912110682ccc8b81492
Cyven <000912143@mohawkcollege.ca>
2025-02-10 11:16:14 -0500

    Setup

---
+++ DesignWeekGroup4/Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public int t1p1Colour;
+    public int t1p1Hat;
+    public int t1p2Colour;
+    public int t1p2Hat;
+
+    public int t2p1Colour;
+    public int t2p1Hat;
+    public int t2p2Colour;
+    public int t2p2Hat;
+
+    public string[] colourOpts;
+    public bool[] colourOptsSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
